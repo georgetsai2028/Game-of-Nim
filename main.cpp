587619a7888e865b9f38f4a3a7e9e0973b87e0d9
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-10-18 12:19:39 -0700

    Rules of game created

--- main.cpp
+++ main.cpp
@@ -11,6 +11,14 @@ using namespace std;
 int main()
 {
 
+    cout << "--------------- THE GAME OF NIM ---------------" << endl;
+    cout << endl;
+    cout << "Rules of the game:" << endl;
+    cout << " * Players take alternate turns taking stones from the pile" << endl;
+    cout << " * Each player remove 1, 2, or 3 stones from the pile." << endl;
+    cout << " * The player who takes the last stone resulting in 0 stones left from the pile loses" << endl;
+
+    
 
    return 0; 
 }
