6cb07fac84ca174f6f6033b5c3628897d111b97c
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-10-23 16:31:37 -0700

    added input for easy or hard mode

--- main.cpp
+++ main.cpp
@@ -10,6 +10,10 @@ using namespace std;
 
 int main()
 {
+    char mode;
+    string e = "You have chosen the easy mode";
+    string h = "You have chosen the hard mode";
+    int count = 0;
 
     cout << "--------------- THE GAME OF NIM ---------------" << endl;
     cout << endl;
@@ -17,6 +21,29 @@ int main()
     cout << " * Players take alternate turns taking stones from the pile" << endl;
     cout << " * Each player remove 1, 2, or 3 stones from the pile." << endl;
     cout << " * The player who takes the last stone resulting in 0 stones left from the pile loses" << endl;
+
+    cout << "Choose mode: " << endl;
+    cout << "Easy Mode (e) or Hard Mode (h): ";
+    
+    cin >> mode;
+
+    while (count < 1)
+    {
+        if (mode != 'e' || mode != 'h')
+        {
+        cout << "Try again, enter (e) for easy more or (h) for hard mode" << endl;
+        cin >> mode;
+        }
+        if (mode == 'e' || mode == 'h')
+        {
+        count++;
+        }
+    }
+
+    if (mode == 'e')
+    {
+        cout << "haha";
+    }
 
     
 
