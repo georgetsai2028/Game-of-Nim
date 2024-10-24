4e3183e57f68d182531767f8a78b1f6decb97c82
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2024-10-24 15:21:28 -0700

    fixed input and making easy function

--- main.cpp
+++ main.cpp
@@ -8,18 +8,44 @@ using namespace std;
 void easy_mode()
 {
     srand(time(0)); //randomly generates need number every time by seeding
+
+    int turn =  0 + rand() % (1 - 0 + 1);
+    int player = 0;
+    int computer = 0;
     int max = 21;
     int min = 10;
     int pile_of_stones = min + rand() % (max - min + 1);
+
     cout << "The initial number of stones is: " << pile_of_stones << endl;
+    cout << endl;
+
+    if (turn == 0)
+    {
+        cout << "Player startss first" << endl;
+        cout << "There are " << pile_of_stones << " left" << endl;
+        cout << "Choose between 1, 2, or 3 stones to take: " << endl;
+        cin >> player;
+        cout << "Player takes " << player << " stones" << endl;
+    }
+    else if (turn == 1)
+    {
+        cout << "Computer starts first" << endl;
+        cout << "There are " << pile_of_stones << " stones left" << endl;
+        computer = 1 + rand() % (3 - 1 +1);
+        pile_of_stones - computer;
+        cout << "Computer takes " << computer << " stones" << endl;
+    }
+
+    
+
+
+
 }
 
 
 int main()
 {
-    string mode = "";
-    string e = "You have chosen the easy mode";
-    string h = "You have chosen the hard mode";
+    char mode;
     int count = 0;
 
     cout << "--------------- THE GAME OF NIM ---------------" << endl;
@@ -30,24 +56,36 @@ int main()
     cout << " * The player who takes the last stone resulting in 0 stones left from the pile loses" << endl;
 
     cout << "Choose mode: " << endl;
-    cout << "Easy Mode (easy) or Hard Mode (hard): ";
+    cout << "Easy Mode (e) or Hard Mode (h): ";
     
     cin >> mode;
 
-    while (count < 1)
+    while (count == 0)
     {
-        if (mode != "easy" || mode != "hard")
+        if (mode == 'e')
         {
-        cout << "Try again, enter (easy) for easy mode or (hard) for hard mode" << endl;
-        cin >> mode;
+            cout << "You have chosen the easy mode" << endl;
+            count++;
         }
-        else if (mode == "easy" || mode == "hard")
+        else if (mode == 'h') 
         {
-        count++;
+            cout << "You have chosen the hard mode" << endl;
+            count++;
+        }
+        else if (mode != 'e')
+        {
+            cout << "Try again, enter (easy) for easy mode or (hard) for hard mode" << endl;
+            cin >> mode;
+        }
+        else if (mode != 'h')
+        {
+            cout << "Try again, enter (easy) for easy mode or (hard) for hard mode" << endl;
+            cin >> mode;
         }
     }
+    
 
-    if (mode == "easy")
+    if (mode == 'e')
     {
         easy_mode();
     }
